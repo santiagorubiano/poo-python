9d53a2d0486e83ea29197fd803f0ecf505ca0c2c
santiago <santiago.rubianom@gmail.com>
2022-10-28 16:11:19 -0500

    terminado

--- morral.py
+++ morral.py
@@ -1,19 +1,22 @@
-def morral( tamaño_morral, pesos,valores , n):
-    
-    if n == 0 or tamaño_morral == 0:
-        return 0
-    
-    if pesos [n - 1] > tamaño_morral:
-        return morral(tamaño_morral,pesos,valores,-n)
-    return max (valores[n-1 ]+morral(tamaño_morral-pesos[n-1],pesos,valores,n-1),
-                morral(tamaño_morral,pesos,valores,n-1))
+def morral(tamano_morral, pesos, valores, n):
 
-if __name__ =='__main__':
-    
-    valores = [60,100,120]
-    pesos= [10,20,30]
-    tamaño_morral = 25
+    if n == 0 or tamano_morral == 0:
+        return 0
+
+    if pesos[n - 1] > tamano_morral:
+        return morral(tamano_morral, pesos, valores, n - 1)
+
+    return max(valores[n - 1] + morral(tamano_morral - pesos[n - 1], pesos, valores, n - 1),
+                morral(tamano_morral, pesos, valores, n - 1))
+        
+
+if __name__ == '__main__':
+    valores = [60, 100, 120]
+    pesos = [10, 20, 30]
+    tamano_morral = 25
     n = len(valores)
-    
-    resultado = morral(tamaño_morral,pesos,valores,n)
+
+    resultado = morral(tamano_morral, pesos, valores, n)
     print(resultado)
+    print(f'El valor maximo que podemos robar es {resultado}')
+    print('La complejidad del algoritmo es O(nW) donde n es el numero de elementos y W el tamano del morral')
---
+++ pruebas.py
@@ -0,0 +1,26 @@
+# def human_years_cat_years_dog_years(human_years):
+    
+#     cat_years = 0
+#     dog_years = 0
+#     if human_years ==1:
+#         cat_years = 15
+#         dog_years = 15
+    
+#     elif human_years ==2:
+#         cat_years = 24
+#         dog_years = 24
+#     elif human_years > 2 :
+#         for i in range(1,human_years):
+        
+#             cat_years = (human_years *4 ) +16
+#             dog_years = (human_years *5) +14
+#     return [human_years,cat_years, dog_years]
+        
+# if __name__== '__main__':
+#     years = int(input("ingrese numero "))
+#     calculo=  human_years_cat_years_dog_years(years)
+#     print(calculo)
+name = 'Juan Salvador'
+name = name.replace('Juan', 'Diego')
+print(name)
+
